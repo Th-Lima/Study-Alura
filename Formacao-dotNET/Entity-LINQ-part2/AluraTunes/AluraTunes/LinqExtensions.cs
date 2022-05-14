194e3bfe7811ff27a1cc889e049edfedd18cf63b
Thales Lima <lthales53@gmail.com>
2022-05-13 23:51:24 -0300

    course linq part 2

---
+++ Formacao-dotNET/Entity-LINQ-part2/AluraTunes/AluraTunes/LinqExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AluraTunes
+{
+    public static class LinqExtensions
+    {
+        public static decimal Mediana<TSource>(this IQueryable<TSource> source, Expression<Func<TSource, decimal>> selector)
+        {
+            var contagem = source.Count();
+
+            var funcSelector = selector.Compile();
+
+            var queryOrdenada = source.Select(funcSelector).OrderBy(total => total);
+
+            var elementoCentral1 = queryOrdenada.Skip(contagem / 2).First();
+
+            var elementoCentral2 = queryOrdenada.Skip((contagem - 1) / 2).First();
+
+            var mediana = (elementoCentral1 + elementoCentral2) / 2;
+
+            return mediana;
+        }
+
+        public static TSource Second<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.Skip(1).First();
+        }
+    }
+}
---
+++ Formacao-dotNET/Entity-LINQ-part2/AluraTunes/AluraTunes/Program.cs
@@ -0,0 +1,120 @@
+﻿using AluraTunes.Data;
+using System;
+using System.Linq;
+
+namespace AluraTunesAula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                decimal queryMedia = contexto.NotaFiscals.Average(x => x.Total);
+                
+                var query = 
+                from nf in contexto.NotaFiscals
+                where nf.Total > queryMedia
+                orderby nf.Total descending
+                select new 
+                {
+                    Numero = nf.NotaFiscalId,
+                    Data = nf.DataNotaFiscal,
+                    Cliente = nf.Cliente.PrimeiroNome + " " + nf.Cliente.Sobrenome,
+                    Valor = nf.Total
+                };
+
+                foreach (var nf in query)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", nf.Numero, nf.Data, nf.Cliente, nf.Valor);
+                }
+
+                Console.WriteLine($"A média é {queryMedia}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// LINQ TO ENTITIES PAGINADO
+    /// </summary>
+    public static class Aula1Video1
+    {
+        private const int TAMANHO_PAGINA = 10;
+       
+        public static void Principal()
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var numeroNotasFiscais = contexto.NotaFiscals.Count();
+                var numeroPaginas = Math.Ceiling((decimal)numeroNotasFiscais / TAMANHO_PAGINA);
+
+                for (int p = 1; p <= numeroPaginas; p++)
+                {
+                    ImprimirPagina(contexto, p);
+                }
+
+            }
+        }
+
+        private static void ImprimirPagina(AluraTunesEntities contexto, int numeroPagina)
+        {
+            var query =
+            from nf in contexto.NotaFiscals
+            orderby nf.NotaFiscalId
+            select new
+            {
+                Numero = nf.NotaFiscalId,
+                Data = nf.DataNotaFiscal,
+                Cliente = nf.Cliente.PrimeiroNome + " " + nf.Cliente.Sobrenome,
+                ValorTotal = nf.Total
+            };
+
+            int numeroDePulos = (numeroPagina - 1) * TAMANHO_PAGINA;
+
+            query = query.Skip(numeroDePulos);
+
+            query = query.Take(TAMANHO_PAGINA);
+
+            Console.WriteLine();
+            Console.WriteLine($"Página {numeroPagina}");
+
+            foreach (var nf in query)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}", nf.Numero, nf.Data, nf.Cliente, nf.ValorTotal);
+            }
+        }
+    }
+
+    /// <summary>
+    /// SUBCONSULTA
+    /// </summary>
+    public static class Aula1Video2
+    {
+        public static void Principal()
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                decimal queryMedia = contexto.NotaFiscals.Average(x => x.Total);
+
+                var query =
+                from nf in contexto.NotaFiscals
+                where nf.Total > queryMedia
+                orderby nf.Total descending
+                select new
+                {
+                    Numero = nf.NotaFiscalId,
+                    Data = nf.DataNotaFiscal,
+                    Cliente = nf.Cliente.PrimeiroNome + " " + nf.Cliente.Sobrenome,
+                    Valor = nf.Total
+                };
+
+                foreach (var nf in query)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", nf.Numero, nf.Data, nf.Cliente, nf.Valor);
+                }
+
+                Console.WriteLine($"A média é {queryMedia}");
+            }
+        }
+    }
+}
