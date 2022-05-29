874b70555fa5970c80d4f2fd01384a4f91d61346
Thales Lima <lthales53@gmail.com>
2022-05-29 17:11:34 -0300

    Entity framework core course part1 initial commit

---
+++ Formacao-dotNET/EntityFrameworkCore-Part1/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SELECT * FROM actor
+
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                foreach (var item in contexto.Atores)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
