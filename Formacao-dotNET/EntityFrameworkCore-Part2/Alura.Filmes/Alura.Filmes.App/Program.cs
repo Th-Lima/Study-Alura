c4c7105f292aee3a9386b55d6818bed2b4cf4993
Thales Lima <lthales53@gmail.com>
2022-06-17 17:49:06 -0300

    entity framework core course part2

---
+++ Formacao-dotNET/EntityFrameworkCore-Part2/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                var ator1 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
+                var ator2 = new Ator { PrimeiroNome = "Emma", UltimoNome = "Watson" };
+                contexto.AddRange(ator1, ator2);
+
+                contexto.SaveChanges();
+
+                var emmaWatson = contexto.Atores.Where(x => x.PrimeiroNome == "Emma" && x.UltimoNome == "Watson");
+                Console.WriteLine($"Quantidade de atores com esse nome encontrados: {emmaWatson.Count()}");
+            }
+        }
+    }
+}
