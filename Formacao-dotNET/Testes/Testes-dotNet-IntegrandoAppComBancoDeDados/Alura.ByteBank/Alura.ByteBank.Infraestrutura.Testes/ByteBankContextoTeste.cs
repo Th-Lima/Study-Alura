66d021a3089b7d73dfb41234877114478aaa8c6c
Thales Lima <lthales53@gmail.com>
2022-08-11 18:32:00 -0300

    Implementing new test

---
+++ Formacao-dotNET/Testes/Testes-dotNet-IntegrandoAppComBancoDeDados/Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTeste.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTeste
+    {
+        [Fact]
+        public void TestaConexaoComBDMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Não foi possível conectar a base de dados", e);
+            }
+
+            //Assert
+            Assert.True(conectado);
+        }
+    }
+}
