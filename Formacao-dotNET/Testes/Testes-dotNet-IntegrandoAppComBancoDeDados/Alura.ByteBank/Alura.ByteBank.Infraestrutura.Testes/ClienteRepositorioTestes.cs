260170bd3975e43f2ac3ef93a4c3006c36f67f03
Thales Lima <lthales53@gmail.com>
2022-08-11 18:44:50 -0300

    Implementing dependency injection on test

--- Formacao-dotNET/Testes/Testes-dotNet-IntegrandoAppComBancoDeDados/Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
+++ Formacao-dotNET/Testes/Testes-dotNet-IntegrandoAppComBancoDeDados/Alura.ByteBank/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -1,18 +1,29 @@
 ﻿using Alura.ByteBank.Dados.Repositorio;
 using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ByteBank.Infraestrutura.Testes
 {
     public class ClienteRepositorioTestes
     {
+        private readonly IClienteRepositorio _clienteRepositorio;
+
+        public ClienteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+
+            _clienteRepositorio = provedor.GetService<IClienteRepositorio>() ?? throw new ArgumentException(nameof(_clienteRepositorio));
+        }
+
         [Fact]
         public void TestaObterTodos()
         {
             //Arrange
-            var _repositorio = new ClienteRepositorio();
-
             //Act 
-            List<Cliente> clientes = _repositorio.ObterTodos();
+            List<Cliente> clientes = _clienteRepositorio.ObterTodos();
 
             var temClientes = clientes.Count > 1;
 
